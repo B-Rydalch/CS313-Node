daddb059ceb2a4f3216e113a0942c9fefeacedb6
Brad Rydalch <bradrydalch@gmail.com>
2018-11-12 14:33:55 -0700

    removed remote and changed origin

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// Learning Node Express
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
