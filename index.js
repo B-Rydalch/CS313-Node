64c0dae4ab98b3e42dd8c9455d65ac25a4141ab7
Brad Rydalch <bradrydalch@gmail.com>
2018-11-14 00:44:29 -0700

    bug searching

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-// Learning Node Express
-
 express()
-  .use(express.static(path.join(__dirname, '/public/')))
+  .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  // .get('/cool', (req, res) => res.send(cool()))
+// .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
